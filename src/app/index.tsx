9583b14832dcb0cf053676b3963b47590c6cb550
unknown <maddewithanagechamuka@gmail.com>
2026-05-25 19:59:05 +0530

    changed styles ddir

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
-
+import { globalStyles } from "../Styles/global";
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MacroZone</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
       <Text style={styles.date}>Monday, March 16</Text>
     </View>
   );
