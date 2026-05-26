5d1652aead767c252e48e58ca93c3be0b4144779
unknown <maddewithanagechamuka@gmail.com>
2026-05-26 14:23:37 +0530

    add meal form

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
 import HomeHeader from "@/components/HomeHeader";
 import MacroGrid from "@/components/MacroGrid";
+import RecentMeals from "@/components/RecentMeals";
 import { globalStyles } from "@/styles/global";
-import { Text, View } from "react-native";
+import { ScrollView, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={globalStyles.container}>
+    <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
       <MacroGrid />
-    </View>
+      <RecentMeals />
+    </ScrollView>
   );
 }
